15532d3a1552a8f8796f7ecdd367fe4683c4f4f3
syedxshah <syedansarofficial@gmail.com>
2026-08-24 15:58:52 +0500

    arrange all files

--- 16.struct/main.go
+++ 16.struct/main.go
@@ -5,50 +5,44 @@ import (
 	"time"
 )
 
-
-type customer struct{
-	name string
+type customer struct {
+	name  string
 	phone string
 }
 
-type order struct{
-
-	id string
-	amount float64
-	status string
-	createdAt time.Time // nano second precision  
-	customer customer
-
-
+type order struct {
+	id        string
+	amount    float64
+	status    string
+	createdAt time.Time // nano second precision
+	customer  customer
 }
 
-
-func (o *order) changeId(id string){
-	o.id = id 
+func (o *order) changeId(id string) {
+	o.id = id
 }
 
 func main() {
 
-		lang := struct {
-			name  string
-			isgood bool
-		}{
+	lang := struct {
+		name   string
+		isgood bool
+	}{
 		"golang", true}
 
-		fmt.Println(lang)
-		my := customer{
-			name: "Ansar",
-			phone: "03240745824",
-		}
-		order := order {
-			id : "a1",
-			amount: 50.00,
-			status: "Available",
-			customer: my,
+	fmt.Println(lang)
+	my := customer{
+		name:  "Ansar",
+		phone: "03240745824",
+	}
+	order := order{
+		id:       "a1",
+		amount:   50.00,
+		status:   "Available",
+		customer: my,
+	}
 
-		}
-		
-		order.createdAt = time.Now()
-		order.changeId("b2")
-		fmt.Println(order)
+	order.createdAt = time.Now()
+	order.changeId("b2")
+	fmt.Println(order)
 }
